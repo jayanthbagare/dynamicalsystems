6f93e438d461ad28ef90a3f94a24e577ed01214b
Jayanth Bagare <jayanth.bagare@gmail.com>
2025-11-24 10:20:43 +0000

    Final commit am tired of fortran for so cumbersome conversions

--- day01_logistic_map/src/exercise1_stable_case.f90
+++ day01_logistic_map/src/exercise1_stable_case.f90
@@ -2,19 +2,18 @@ module exercise1
    implicit none
 
 contains
-   subroutine stable_case(r,x)
-      real, intent(in) :: r,x
-      ! real, parameter,intent(in) :: r,x
+   subroutine stable_case(r_in,x_in)
+      real, intent(in) :: r_in
+      real, intent(in) :: x_in
       integer :: i
-
+      real :: x 
       character(len=50) :: filename
       integer :: iostat_val, unit
       character(len=100) :: iostat_msg
 
-      print *, 'r:', r
-      print *, 'x:', x
-      ! filename = 'data','r_',trim(r),'x_',trim(x),'.csv'
-      filename = 'data.csv'
+      x = x_in 
+      filename = 'data'//'_'//r_in//'_'//x//'.csv'
+      print *, filename
       open (newunit=unit, file=filename, status='replace', action='write', iostat=iostat_val, iomsg=iostat_msg)
       if (iostat_val /= 0) then
          print *, 'Error opening file', iostat_msg, ",", iostat_val
@@ -24,7 +23,7 @@ contains
       end if
 
       do i = 1, 100
-         x = r*x*(1 - x)
+         x = r_in*x*(1 - x)
          write (unit, '(G0.5,",",G0.5)') i, x
       end do
 
