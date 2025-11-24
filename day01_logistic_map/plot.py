b0009fda82a20f45ba82bd9184fb8f560c1fbfd3
Jayanth Bagare <jayanth.bagare@gmail.com>
2025-11-24 11:41:22 +0000

    Cleaned commit

--- day01_logistic_map/plot.py
+++ day01_logistic_map/plot.py
@@ -5,15 +5,17 @@ import os
 
 
 csv_files = glob.glob(os.path.join("./", "*.csv"))
+
 for file_path in csv_files:
     x = file_path.split("_")[2].split(".")[1]
     r = file_path.split("_")[1]
     df = pd.read_csv(file_path)
 
     plt.plot(df["n"], df["x"])
-    plt.title("Logistic Growth")
+    plt.title("Logistic Growth_" + r + "_" + x)
     plt.xlabel("Time Step")
     plt.ylabel("X Value")
 
     plt.savefig("plot" + "_" + r + "_" + x + ".png")
+    plt.close()
     del df
