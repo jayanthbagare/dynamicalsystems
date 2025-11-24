c8f5d012b260cc56ca3e3a8bbfab0cbb2b274ac7
Jayanth Bagare <jayanth.bagare@gmail.com>
2025-11-24 10:05:17 +0000

    Commit with errors

--- day01_logistic_map/app/main.f90
+++ day01_logistic_map/app/main.f90
@@ -2,5 +2,5 @@ program main
   use exercise1 
   implicit none
 
-  call stable_case()
+  call stable_case(2.8,0.3)
 end program main
--- day01_logistic_map/src/exercise1_stable_case.f90
+++ day01_logistic_map/src/exercise1_stable_case.f90
@@ -2,15 +2,18 @@ module exercise1
    implicit none
 
 contains
-   subroutine stable_case
-      real, parameter :: r = 2.8
-      real :: x
+   subroutine stable_case(r,x)
+      real, intent(in) :: r,x
+      ! real, parameter,intent(in) :: r,x
       integer :: i
 
-      character(len=20) :: filename
+      character(len=50) :: filename
       integer :: iostat_val, unit
       character(len=100) :: iostat_msg
 
+      print *, 'r:', r
+      print *, 'x:', x
+      ! filename = 'data','r_',trim(r),'x_',trim(x),'.csv'
       filename = 'data.csv'
       open (newunit=unit, file=filename, status='replace', action='write', iostat=iostat_val, iomsg=iostat_msg)
       if (iostat_val /= 0) then
@@ -19,7 +22,7 @@ contains
        else 
          write (unit, '(A,",",A)') "n", "x"
       end if
-      x = 0.3
+
       do i = 1, 100
          x = r*x*(1 - x)
          write (unit, '(G0.5,",",G0.5)') i, x
